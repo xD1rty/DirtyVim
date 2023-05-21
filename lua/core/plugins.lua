a844e447f83277ffdb5e4693470dc587ce7d4809
Ivan Kriventsev <dirtyhornet277@gmail.com>
2023-05-21 13:11:19 +0000

    Lazy.Nvim Plugins

---
+++ lua/core/plugins.lua
@@ -0,0 +1,63 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{ "phaazon/hop.nvim" },
+	{
+        "nvim-neo-tree/neo-tree.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim", "s1n7ax/nvim-window-picker"
+        }
+    },
+    	{ "nvim-treesitter/nvim-treesitter" },
+	{ "neovim/nvim-lspconfig" },
+	{ "morhetz/gruvbox" },
+    {'hrsh7th/cmp-nvim-lsp'}, {'hrsh7th/cmp-buffer'}, {'hrsh7th/cmp-path'},
+    {'hrsh7th/cmp-cmdline'}, {'hrsh7th/nvim-cmp'},
+    {
+   "williamboman/mason.nvim",
+   build = ":MasonUpdate"
+},
+{
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+-- or                              , branch = '0.1.1',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    { "jose-elias-alvarez/null-ls.nvim" },
+    {
+  'glepnir/dashboard-nvim',
+  event = 'VimEnter',
+
+  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+},
+{'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      -- animation = true,
+      -- insert_at_start = true,
+      -- …etc.
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  },
+  {
+    "nvim-lualine/lualine.nvim"
+  }
+})
+
